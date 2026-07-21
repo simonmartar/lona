f652bd99d20c21f3f1b32283bc2df8c28379bcdf
simonmartar <simonmart@tutamail.com>
2026-07-21 21:07:47 +0300

    Add Disk class with initialization parameters

---
+++ disk.py
@@ -0,0 +1,14 @@
+class Disk:
+
+    def __init__(
+        self,
+        name,
+        total,
+        used
+    ):
+
+        self.name = name
+
+        self.total = total
+
+        self.used = used
